9756153cfe0c07907a096993c26026a6885b1470
Harley <Harley-Mortimer@live.nmit.ac.nz>
2022-03-09 13:59:43 +1300

    :hamburger: updated notes :hamburger:

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Hello World!") 
+// TODO: 
+
+// NOTE:
+/* wecreated a "src" folder added a app.js file. We then changed the package.jason file to make app.js our main. then typed "cd src" "node app.js"
+"git add ." "git commit -m" "git push origin main"
+
+
+*/
