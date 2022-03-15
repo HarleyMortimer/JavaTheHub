27dc513a641460c5be65163ad483d6d9ddb5630d
Harley <Harley-Mortimer@live.nmit.ac.nz>
2022-03-16 09:35:54 +1300

    variables

---
+++ src/week-4.js
@@ -0,0 +1,15 @@
+//NOTE
+/* Var = Variables 
+Exit nodemon = "Ctrl C"
+*/
+
+//TODO
+/*
+JavaScript Variables
+*/
+
+var FullName = "Harley Mortimer"
+var classCode = 503
+
+console.log(FullName)
+console.log(classCode)
