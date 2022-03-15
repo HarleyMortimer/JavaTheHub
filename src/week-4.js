957513fd85ff1f34d1fa97b24dd44e87e363fa41
Harley <Harley-Mortimer@live.nmit.ac.nz>
2022-03-16 11:02:04 +1300

    javascript

--- src/week-4.js
+++ src/week-4.js
@@ -1,6 +1,10 @@
 //NOTE
 /* Var = Variables 
+Enter nodemon = "npm run dev"
 Exit nodemon = "Ctrl C"
+{} = Object
+using a "." or [""] in console reads sub-catagory
+
 */
 
 //TODO
@@ -13,3 +17,40 @@ var classCode = 503
 
 console.log(FullName)
 console.log(classCode)
+
+// JavaScript Objectives
+var full_name = 
+{
+  first_name: "Harley",
+  last_name:  "Mortimer",
+  course_number: 503
+}
+
+console.log(full_name)
+console.log(full_name.first_name)
+console.log(full_name["last_name"])
+
+//JavaScript Functions
+function addTwoNumbers(num1, num2) {
+return num1+num2
+}
+console.log(addTwoNumbers(2,3))
+
+function addThreeNumbers(num1, num2, num3) {
+    return num1+num2+num3
+}
+
+console.log(addThreeNumbers(4.3,2.3,6))
+
+//JavaScript Loops
+for(var i=0; i< 10; i++) {
+    console.log(i)
+}
+
+// JavaScript if statements
+var number = 9
+if (number === 10){
+  console.log("answer = " + number)
+} else {
+  console.log("error")
+}
