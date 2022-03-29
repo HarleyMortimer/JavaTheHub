125d6ca24b772c46c8261477ee6696e02966c117
Harley <Harley-Mortimer@live.nmit.ac.nz>
2022-03-30 11:04:33 +1300

    :smile:

--- src/console.js
+++ src/console.js
@@ -1,4 +1,13 @@
 console.log("Hello World")
 console.dir(document)
 console.table({first:"test",val:9});
+// ReferenceError: document is not defined
+//at Object.<anonymous> (C:\Users\Harle\workspace\JavaTheHub\src\week-6.js:2:13)
+//at Module._compile (node:internal/modules/cjs/loader:1103:14)
+//at Object.Module._extensions..js (node:internal/modules/cjs/loader:1155:10)
+//at Module.load (node:internal/modules/cjs/loader:981:32)
+//at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+//at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)
+//at node:internal/main/run_main_module:17:47
+//[nodemon] app crashed - waiting for file changes before starting...
 console.error("Hello World")
---
+++ src/week-6.js
@@ -0,0 +1,15 @@
+//console.log("hello world")
+//var a = "hello"
+//a
+//console.log(a);
+
+
+var myName = "Harley";
+var myCourse = "JavaScript";
+console.log(myName);
+//alert("Welcome");
+var myAge = 26;
+console.log(myAge)
+
+
+
