f3e587279a99aaf56b30766282fcdc08cecd3893
Harley <Harley-Mortimer@live.nmit.ac.nz>
2022-03-30 11:43:34 +1300

    yo

--- src/week-6.js
+++ src/week-6.js
@@ -4,14 +4,19 @@
 //console.log(a);
 
 
-var myName = "Harley";
-var myCourse = "JavaScript";
-console.log(myName);
+//var myName = "Harley";
+//var myCourse = "JavaScript";
+//console.log(myName);
 //alert("Welcome");
-var myAge = 26; //Thats my real age baby...
-console.log(myAge)
+//var myAge = 26; //Thats my real age baby...
+//console.log(myAge)
 /*
 */ 
 
+var welcome = "Welcome to the house of fun.." //welcome message
+var friendsName = "Jimi Russell" //friends name
+console.log(welcome) // output message
+console.log(friendsName) // output name
+console.log(welcome, friendsName)
 
 
